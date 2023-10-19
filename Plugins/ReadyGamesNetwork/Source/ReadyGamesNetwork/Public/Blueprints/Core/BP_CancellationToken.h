3147cdae48da2b14caf8cf9ee6330b03d1954c55
Arthur <car3man@gmail.com>
2023-10-19 15:25:34 +0300

    ~ Dummy BP_CancellationToken and BP_IMessageReceiver

---
+++ Plugins/ReadyGamesNetwork/Source/ReadyGamesNetwork/Public/Blueprints/Core/BP_CancellationToken.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BP_CancellationToken.generated.h"
+
+USTRUCT(BlueprintType)
+struct READYGAMESNETWORK_API FBP_CancellationToken
+{
+    GENERATED_BODY()
+};
---
+++ Plugins/ReadyGamesNetwork/Source/ReadyGamesNetwork/Public/Blueprints/Core/BP_IMessageReceiver.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BP_IMessageReceiver.generated.h"
+
+USTRUCT(BlueprintType)
+struct READYGAMESNETWORK_API FBP_IMessageReceiver
+{
+    GENERATED_BODY()
+};
